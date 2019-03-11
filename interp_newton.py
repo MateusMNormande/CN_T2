7a378235d6c519d7e1db7ec38abe5a4c936e92f8
lucasuchoa98 <38196129+lucasuchoa98@users.noreply.github.com>
2019-03-11 15:21:17 -0300

    Update interp_newton.py

--- interp_newton.py
+++ interp_newton.py
@@ -25,3 +25,10 @@ valores_y = [15.0,8.0,-1.0] #list(map(float,input("Digite os valores de y: ").sp
 matriz = gera_matriz(qntd_pontos,valores_y)
 x = 2.0 #float(input("Informe os valores a serem interpolados"))
 fs = calcula_f(qntd_pontos,vetor_x,matriz)
+x0 = (vetor_x[0])*(-1)
+x1 = (vetor_x[1])*(-1)
+xx1 = P([x0,1])
+xx2 = P([x1,1])
+p1= P(valores_y)
+p2 = P(fs[0]) + P(fs[1])*xx1 + P(fs[2])*xx1*xx2
+print(p2(2))
