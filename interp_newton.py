c50bbd1623fb1e93efece12ff91f973756c893e3
lucasuchoa98 <38196129+lucasuchoa98@users.noreply.github.com>
2019-03-11 15:30:39 -0300

    Update interp_newton.py

--- interp_newton.py
+++ interp_newton.py
@@ -19,16 +19,19 @@ def calcula_f(qntd_pontos,vetor_x,matriz):
     diagonal = list(np.diagonal(matriz))
     return diagonal
 
-qntd_pontos = 3 #int(input("Digite a quantida de pontos: "))
-vetor_x = [-1.0,0.0,3.0] #list(map(float,input("Digite os valores de x: ").split()))
-valores_y = [15.0,8.0,-1.0] #list(map(float,input("Digite os valores de y: ").split()))
+def calcula_polinomio(qntd_pontos,fs,vetor_x,x):
+    grau = qntd_pontos - 1
+    p = fs[grau]
+    for k in range(1,qntd_pontos):
+        p = fs[grau-k] + (x -vetor_x[grau-k])*p
+    return p
+    
+
+qntd_pontos = int(input("Digite a quantida de pontos: "))
+vetor_x = list(map(float,input("Digite os valores de x: ").split()))
+valores_y = list(map(float,input("Digite os valores de y: ").split()))
 matriz = gera_matriz(qntd_pontos,valores_y)
-x = 2.0 #float(input("Informe os valores a serem interpolados"))
+x = float(input("Informe o valor a ser interpolado: "))
 fs = calcula_f(qntd_pontos,vetor_x,matriz)
-x0 = (vetor_x[0])*(-1)
-x1 = (vetor_x[1])*(-1)
-xx1 = P([x0,1])
-xx2 = P([x1,1])
-p1= P(valores_y)
-p2 = P(fs[0]) + P(fs[1])*xx1 + P(fs[2])*xx1*xx2
-print(p2(2))
+p = calcula_polinomio(qntd_pontos,fs,vetor_x,x)
+print("p(%f) = %f"%(x,p))
